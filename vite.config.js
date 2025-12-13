99e2193bb472f90dd8b5ac72e3e616e9fd9f20bb
박성원 <sungwon4867@naver.com>
2025-12-14 03:28:24 +0900

    지도 앱 완성본(map.html) 추가

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +7,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'map.html'), // 여기가 핵심! map.html을 메인으로 설정
+        main: 'map.html',
       },
     },
   },
